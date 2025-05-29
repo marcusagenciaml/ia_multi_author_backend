09ad92a8555338c48228cae3d33cc596548aa41b
Marcus Robalino <marcusrobalino@agenciaml.com>
2025-05-29 20:16:38 -0300

    Alteração do LLM para o deepseek-r1:free

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    OPENROUTER_API_KEY: str
+    OPENROUTER_API_KEY: str = ""
     EMBEDDING_MODEL_NAME: str = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
-    LLM_MODEL_NAME: str = "mistralai/mistral-7b-instruct-v0.2" # Comece com um modelo confiável
+    LLM_MODEL_NAME: str = "deepseek/deepseek-r1:free" # Comece com um modelo confiável
     FAISS_INDEX_PATH: str = "faiss_index_multi_author"
 
     # Para carregar do arquivo .env
